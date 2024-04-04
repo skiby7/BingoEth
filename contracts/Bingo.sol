fcb17a0d9d7de043ef656186f2f322df25bbb548
Leonardo Manneschi <leomanne2000@gmail.com>
2024-04-04 19:50:34 +0200

    prova

--- contracts/Bingo.sol
+++ contracts/Bingo.sol
@@ -1,17 +1,16 @@
-pragma solidity ^0.8.0;
-
+pragma solidity ^0.8.19;
 contract Bingo {
     struct info {
-        address creator;
-        address[] joiners;
-        uint maxJoiners;
-        uint totalJoiners;
-        uint ethBalance;
-        uint betAmount;
-        bytes32 creatorMerkleRoot;
-        mapping(address => bytes32) joinerMerkleRoots;
-        uint accusationTime;
-        address accuser;
+    address creator;
+    address[] joiners;
+    uint maxJoiners;
+    uint totalJoiners;
+    uint ethBalance;
+    uint betAmount;
+    bytes32 creatorMerkleRoot;
+    bytes32 joinerMerkleRoots; //TODO: implementa piu persone
+    uint accusationTime;
+    address accuser;
     }
     uint256 public gameId = 0; // Game ID counter
     mapping(uint256 => info) public gameList; // Mapping of game ID to game info
@@ -23,32 +22,34 @@ contract Bingo {
     constructor() {
         // Add constructor code
     }
-
-    function createGame(uint _maxJoiners, uint _betAmount, bytes32 _creatorMerkleRoot)  public payable {
+    
+    function createGame(uint _maxJoiners, uint _betAmount)  public payable {
         require(_maxJoiners > 0, "Max joiners must be greater than 0");
         require(_betAmount > 0, "Bet amount must be greater than 0");
-        require(_creatorMerkleRoot != bytes32(0), "Invalid creator merkle root");
 
     
-        uint256[] memory gameID = getIDGame();
-        gameList[gameID] = info({
-            creator: msg.sender,
-            joiners: new address[](0),
-            maxJoiners: _maxJoiners,
-            totalJoiners: 0,
-            ethBalance: 0,
-            betAmount: _betAmount,
-            creatorMerkleRoot: _creatorMerkleRoot,
-            accusationTime: 0,
-            accuser: address(0)
-        });
+        uint256 gameID = getIDGame();
+        info memory newGame;
+        newGame.creator = msg.sender;
+        newGame.joiners = new address[](0);
+        newGame.maxJoiners = _maxJoiners;
+        newGame.totalJoiners = 0;
+        newGame.ethBalance = 0;
+        newGame.betAmount = _betAmount;
+        newGame.creatorMerkleRoot = 0;
+        newGame.joinerMerkleRoots = 0; //TODO: aggiorna per piu utenti
+        newGame.accusationTime = 0;
+        newGame.accuser = address(0);
+       
+        gameList[gameID] = newGame;
+           
         availableGames.push(gameID);
         gameList[gameID].ethBalance += msg.value;
         emit GameCreated(gameID);
     }
     
 
-    function getIDGame() private returns(uint256[] memory) {
+    function getIDGame() private returns(uint256 ) {
         return ++gameId;
     }
     function join(uint256 _gameId) external{
--- src/js/app.js
+++ src/js/app.js
@@ -13,40 +13,36 @@ App = {
       // User denied account access...
       console.error("User denied account access")
     }
-}
-// Legacy dapp browsers...
-else if (window.web3) {
-  App.web3Provider = window.web3.currentProvider;
-}
-// If no injected web3 instance is detected, fall back to Ganache
-else {
-  App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
-}
-web3 = new Web3(App.web3Provider);
-
-
+    }
+    // Legacy dapp browsers...
+    else if (window.web3) {
+      App.web3Provider = window.web3.currentProvider;
+    }
+    // If no injected web3 instance is detected, fall back to Ganache
+    else {
+      App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
+    }
+    web3 = new Web3(App.web3Provider);
     return App.initContract();
   },
 
   initContract: function() {
-    $.getJSON('Adoption.json', function(data) {
+    $.getJSON('Bingo.json', function(data) {
       // Get the necessary contract artifact file and instantiate it with @truffle/contract
-      var AdoptionArtifact = data;
-      App.contracts.Adoption = TruffleContract(AdoptionArtifact);
+      var BingoArtifact = data;
+      App.contracts.Bingo = TruffleContract(BingoArtifact);
     
       // Set the provider for our contract
-      App.contracts.Adoption.setProvider(App.web3Provider);
-    
-      // Use our contract to retrieve and mark the adopted pets
-      return App.markAdopted();
+      App.contracts.Bingo.setProvider(App.web3Provider);
     });
-    
-
     return App.bindEvents();
   },
 
   bindEvents: function() {
-    $(document).on('click', '.btn-adopt', App.handleAdopt);
+    //$(document).on('click', '.btn-adopt', App.handleAdopt);
+    $(document).on('click', '#createNewGameBtn', App.handleCreateRoom);
+
+
   },
 
   markAdopted: function() {
@@ -90,10 +86,54 @@ web3 = new Web3(App.web3Provider);
         });
     });
 
+  },
+  
+  gameCreation: function () {
+    boardSize = 5; // Bingo board size
+    iHostTheGame = true;
+  
+    // Call to the contract
+    App.contracts.Bingo.deployed().then(async function (instance) {
+      newInstance = instance;
+      return newInstance.createGame({ value: 0 }); // No value needed for bingo
+    }).then(async function (logArray) { // Callback to the contract function createGame
+      gameId = logArray.logs[0].args._gameId.toNumber(); // Get the gameId from the event emitted in the contract
+      if (gameId < 0) {
+        console.error("Something went wrong, game id is negative!");
+      }
+      else {
+        // Waiting room:
+        $('#newGame').hide();
+        $('#waitingOpponent').show();
+        document.getElementById('waitingOpponentConnection').innerHTML = "<h2>Creation of a Bingo board.</h2>" +
+          "<h2>Waiting for an opponent! The Game ID is " + gameId + "!</h2>";
+  
+        // Board matrix initialization
+        bingoBoard = [];
+  
+        for (var i = 0; i < boardSize; i++) {
+          bingoBoard[i] = [];
+          for (var j = 0; j < boardSize; j++) {
+            bingoBoard[i][j] = 0;
+          }
+        }
+  
+        // Waiting for the opponent:
+        App.handleEvents();
+      }
+    }).catch(function (err) {
+      console.log(err.message);
+    });
+  },
+  handleCreateRoom: function () { // function to show the create game menu
+    $('#newGame').show();
+    $('#initialsection').hide();
   }
-
 };
-
+function loader()
+{
+    document.getElementById("guscioloader").style.display="flex";
+}
 $(function() {
   $(window).load(function() {
     App.init();
