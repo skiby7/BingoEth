b3a8c8a3f4eb87cb33609ce064a172efe509c8eb
Skiby7 <l.scoppitto@studenti.unipi.it>
2024-05-15 23:04:49 +0200

    problem with gas limit

--- truffle/truffle-config.js
+++ truffle/truffle-config.js
@@ -5,7 +5,8 @@ module.exports = {
     development: {
       host: "127.0.0.1",
       port: 7545,
-      network_id: "*" // Match any network id
+      network_id: "*", // Match any network id
+      gasLimit: 100000000000
     },
     develop: {
       port: 7545
