29a4dc2872fa8da8779b81bd3b0238ae767067a8
Skiby7 <l.scoppitto@studenti.unipi.it>
2024-06-24 00:26:02 +0200

    mobile style

--- client/src/components/MainView.js
+++ client/src/components/MainView.js
@@ -13,20 +13,21 @@ const MainView = () => {
 
   return (
     <div className="flex flex-col min-h-screen">
-      <div className="top-0 w-1/2 m-auto h-16 mt-7 rounded-3xl dark:bg-blue-500 bg-blue-300 shadow-lg flex justify-center items-center">
+      <div className="w-1/2 m-auto h-16 mt-7 rounded-3xl dark:bg-blue-500 bg-blue-300 shadow-lg flex justify-center items-center">
         <div className="text-black dark:text-white text-lg lg:text-2xl">Bingo ETH</div>
       </div>
+
       {view === '' && (
-        <div className="flex-grow flex justify-center items-center pt-[5em]">
+        <div className="flex-grow flex flex-col justify-center items-center pt-[5em]">
           <div className="gap-2">
             <div className="flex flex-col lg:flex-row gap-4 mb-10">
-              <button className="tile" onClick={() => { setView('createRoom'); }}>
+              <button className="tile" onClick={() => setView('createRoom')}>
                 Crea stanza
               </button>
-              <button className="tile" onClick={() => { setView('joinRandomGame'); }}>
+              <button className="tile" onClick={() => setView('joinRandomGame')}>
                 Entra in una stanza random
               </button>
-              <button className="tile" onClick={() => { setView('joinGame'); }}>
+              <button className="tile" onClick={() => setView('joinGame')}>
                 Entra in una stanza
               </button>
             </div>
@@ -56,23 +57,27 @@ const MainView = () => {
           </div>
         </div>
       )}
+
       {view === 'createRoom' && (
-        <div className="flex justify-center items-center h-[calc(100vh-6.5rem)] w-screen">
+        <div className="flex flex-grow justify-center items-center w-screen mt-5 mb-10">
           <CreateRoom setView={setView} />
         </div>
       )}
+
       {view === 'joinRandomGame' && (
-        <div className="flex justify-center items-center h-[calc(100vh-6.5rem)] w-screen">
+        <div className="flex flex-grow justify-center items-center w-screen mt-5 mb-10">
           <JoinGame setView={setView} randomGame={true} />
         </div>
       )}
+
       {view === 'joinGame' && (
-        <div className="flex justify-center items-center h-[calc(100vh-6.5rem)] w-screen">
+        <div className="flex flex-grow justify-center items-center w-screen mt-5 mb-10">
           <JoinGame setView={setView} randomGame={false} />
         </div>
       )}
     </div>
   );
+
 
 
 };
